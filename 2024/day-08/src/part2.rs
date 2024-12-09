d40dce1f7a4e30a8778595e2b8db0292b6373eed
WataNekko <88185666+WataNekko@users.noreply.github.com>
2024-12-10 00:06:02 +0700

    Solve day 8 part 2

--- 2024/day-08/src/part2.rs
+++ 2024/day-08/src/part2.rs
@@ -1,6 +1,55 @@
+use std::{collections::HashMap, iter::successors};
+
+use glam::IVec2;
+use itertools::Itertools;
+
+fn parse(input: &str) -> (HashMap<char, Vec<IVec2>>, IVec2) {
+    let mut bound = IVec2::ZERO;
+    let mut antenna_groups: HashMap<_, Vec<_>> = HashMap::new();
+
+    for (y, line) in input.lines().enumerate() {
+        bound.y = bound.y.max(y as _);
+        for (x, ch) in line.chars().enumerate() {
+            bound.x = bound.x.max(x as _);
+
+            if ch.is_alphanumeric() {
+                antenna_groups
+                    .entry(ch)
+                    .or_default()
+                    .push(IVec2::new(x as _, y as _));
+            }
+        }
+    }
+
+    (antenna_groups, bound)
+}
+
+fn is_within_bound(pos: IVec2, bound: IVec2) -> bool {
+    IVec2::ZERO.cmple(pos).all() && pos.cmple(bound).all()
+}
+
 #[tracing::instrument]
-pub fn process(_input: &str) -> miette::Result<String> {
-    todo!("day 01 - part 2");
+pub fn process(input: &str) -> miette::Result<usize> {
+    let (antenna_groups, bound) = parse(input);
+
+    let count = antenna_groups
+        .values()
+        .flat_map(|pos_group| {
+            pos_group.iter().copied().permutations(2).flat_map(|pair| {
+                let &[a, b] = pair.as_slice() else {
+                    panic!("expect permutation of 2");
+                };
+
+                let move_vec = a - b;
+                successors(Some(a), move |&prev| {
+                    Some(prev + move_vec).filter(|&pos| is_within_bound(pos, bound))
+                })
+            })
+        })
+        .unique()
+        .count();
+
+    Ok(count)
 }
 
 #[cfg(test)]
@@ -10,9 +59,19 @@ mod tests {
     #[test]
     fn test_process() -> miette::Result<()> {
         tracing_subscriber::fmt::init();
-        todo!("haven't built test yet");
-        let input = "";
-        assert_eq!("", process(input)?);
+        let input = "............
+........0...
+.....0......
+.......0....
+....0.......
+......A.....
+............
+............
+........A...
+.........A..
+............
+............";
+        assert_eq!(34, process(input)?);
         Ok(())
     }
 }
