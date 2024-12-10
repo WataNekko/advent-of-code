cfed7072a309f561b1443ef643a535eb204d2db7
WataNekko <88185666+WataNekko@users.noreply.github.com>
2024-12-11 01:22:53 +0700

    Solve day 9 part 2

--- 2024/day-09/src/part2.rs
+++ 2024/day-09/src/part2.rs
@@ -1,6 +1,46 @@
 #[tracing::instrument]
-pub fn process(_input: &str) -> miette::Result<String> {
-    todo!("day 01 - part 2");
+pub fn process(input: &str) -> miette::Result<usize> {
+    let (mut files, mut frees, _) = input
+        .lines()
+        .next()
+        .unwrap()
+        .chars()
+        .map(|ch| {
+            ch.to_digit(10)
+                .unwrap_or_else(|| panic!("'{}' should be digit", ch))
+        })
+        .enumerate()
+        .fold(
+            (Vec::new(), Vec::new(), 0),
+            |(mut files, mut frees, block_index), (i, block_count)| {
+                if i % 2 == 0 { &mut files } else { &mut frees }
+                    .push(block_index..block_index + block_count);
+                (files, frees, block_index + block_count)
+            },
+        );
+
+    for file in files.iter_mut().rev() {
+        for free in frees.iter_mut() {
+            if free.start > file.start {
+                break;
+            }
+
+            if free.len() >= file.len() {
+                *file = free.start..free.start + file.len() as u32;
+                free.start += file.len() as u32;
+                break;
+            }
+        }
+    }
+
+    let checksum = files
+        .iter()
+        .cloned()
+        .enumerate()
+        .map(|(id, file_range)| id * file_range.sum::<u32>() as usize)
+        .sum();
+
+    Ok(checksum)
 }
 
 #[cfg(test)]
@@ -10,9 +50,8 @@ mod tests {
     #[test]
     fn test_process() -> miette::Result<()> {
         tracing_subscriber::fmt::init();
-        todo!("haven't built test yet");
-        let input = "";
-        assert_eq!("", process(input)?);
+        let input = "2333133121414131402";
+        assert_eq!(2858, process(input)?);
         Ok(())
     }
 }
