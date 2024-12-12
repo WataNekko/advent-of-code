783415b57bcd5cc6896990567e056bd3cb5526df
WataNekko <88185666+WataNekko@users.noreply.github.com>
2024-12-13 02:14:37 +0700

    Solve day 11 part 2

--- 2024/day-11/src/part2.rs
+++ 2024/day-11/src/part2.rs
@@ -1,6 +1,50 @@
+use std::collections::HashMap;
+
+use itertools::Itertools;
+use miette::miette;
+use nom::{
+    character::complete::{self, space1},
+    multi::separated_list1,
+    IResult,
+};
+use num_traits::Euclid;
+
+fn parse(input: &str) -> IResult<&str, Vec<u64>> {
+    separated_list1(space1, complete::u64)(input)
+}
+
 #[tracing::instrument]
-pub fn process(_input: &str) -> miette::Result<usize> {
-    todo!("day 01 - part 2");
+pub fn process(input: &str, blinks: u32) -> miette::Result<usize> {
+    let (_, stones) = parse(input).map_err(|e| miette!("parse failed {}", e))?;
+    let stones = stones.iter().copied().counts();
+
+    let count = (0..blinks)
+        .fold(stones, |prev, _| {
+            let mut new = HashMap::new();
+            prev.iter().for_each(|(&num, &count)| match num {
+                0 => {
+                    new.entry(1).and_modify(|v| *v += count).or_insert(count);
+                }
+                n => {
+                    let digit_count = n.checked_ilog10().unwrap_or(0) + 1;
+                    if digit_count % 2 == 0 {
+                        let (a, b) = n.div_rem_euclid(&10u64.pow(digit_count / 2));
+                        for n in [a, b] {
+                            new.entry(n).and_modify(|v| *v += count).or_insert(count);
+                        }
+                    } else {
+                        new.entry(n * 2024)
+                            .and_modify(|v| *v += count)
+                            .or_insert(count);
+                    }
+                }
+            });
+            new
+        })
+        .values()
+        .sum();
+
+    Ok(count)
 }
 
 #[cfg(test)]
@@ -10,9 +54,8 @@ mod tests {
     #[test]
     fn test_process() -> miette::Result<()> {
         tracing_subscriber::fmt::init();
-        todo!("haven't built test yet");
-        let input = "";
-        assert_eq!(0, process(input)?);
+        let input = "125 17";
+        assert_eq!(55312, process(input, 25)?);
         Ok(())
     }
 }
