39f4a5c97dcda77c178bd3633eaa4c37615ccc4a
WataNekko <88185666+WataNekko@users.noreply.github.com>
2024-12-13 00:16:59 +0700

    Change daily template return type to usize

--- 2024/daily-template/src/part2.rs
+++ 2024/daily-template/src/part2.rs
@@ -1,5 +1,5 @@
 #[tracing::instrument]
-pub fn process(_input: &str) -> miette::Result<String> {
+pub fn process(_input: &str) -> miette::Result<usize> {
     todo!("day 01 - part 2");
 }
 
@@ -12,7 +12,7 @@ mod tests {
         tracing_subscriber::fmt::init();
         todo!("haven't built test yet");
         let input = "";
-        assert_eq!("", process(input)?);
+        assert_eq!(0, process(input)?);
         Ok(())
     }
 }
