bcf8dca6328702cefde2cccf617c04a94bcc0f70
WataNekko <88185666+WataNekko@users.noreply.github.com>
2024-12-17 23:42:29 +0700

    Solve day 13 part 1

---
+++ 2024/day-13/src/part2.rs
@@ -0,0 +1,18 @@
+#[tracing::instrument]
+pub fn process(_input: &str) -> miette::Result<usize> {
+    todo!("day 01 - part 2");
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_process() -> miette::Result<()> {
+        tracing_subscriber::fmt::init();
+        todo!("haven't built test yet");
+        let input = "";
+        assert_eq!(0, process(input)?);
+        Ok(())
+    }
+}
